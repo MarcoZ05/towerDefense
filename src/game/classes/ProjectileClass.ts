0b4723259310d95fa1e4f33e6f4e5cd1124097f6
Käkäsh <marco.zillgen23@gmail.com>
2022-08-15 18:26:04 +0200

    some major changes of Projectiles

--- src/game/classes/ProjectileClass.ts
+++ src/game/classes/ProjectileClass.ts
@@ -1,40 +1,17 @@
 import { AttackInterface, PositionInterface } from '../interfaces'
 
 export default class ProjectileClass {
+  angle: number
   position: PositionInterface
-  size: number
-  duration: {
-    time: number
-    enemies: number
-  }
-  type: string
-  damage: number
-  deltaPosition: PositionInterface
+  attack: AttackInterface
   constructor (
-    towerPosition: PositionInterface,
-    enemyPosition: PositionInterface,
-    size: number,
+    angle: number,
+    position: PositionInterface,
     attack: AttackInterface
   ) {
-    this.position = towerPosition
-    this.size = size
-    this.duration = attack.duration
-    this.type = attack.type
-    this.damage = attack.damage
-    this.deltaPosition = {
-      x:
-        (attack.speed * (enemyPosition.x - towerPosition.x)) /
-        Math.sqrt(
-          Math.pow(enemyPosition.x - towerPosition.x, 2) +
-            Math.pow(enemyPosition.y - towerPosition.y, 2)
-        ),
-      y:
-        (attack.speed * (enemyPosition.y - towerPosition.y)) /
-        Math.sqrt(
-          Math.pow(enemyPosition.x - towerPosition.x, 2) +
-            Math.pow(enemyPosition.y - towerPosition.y, 2)
-        )
-    }
+    this.angle = angle
+    this.position = position
+    this.attack = attack
   }
 
   move (): void {
