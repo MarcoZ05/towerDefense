fba3a5653c377fbe7d07e2f61132fb18072725c3
Käkäsh <marco.zillgen23@gmail.com>
2022-08-15 18:13:25 +0200

    more explicit type

--- src/game/interfaces/AttackInterface.ts
+++ src/game/interfaces/AttackInterface.ts
@@ -1,18 +1,9 @@
-export interface AttackInterface {
+export type AttackInterface = setAttackInterface | trackingAttackInterface
+
+interface basicAttackInterface {
   damage: number
   speed: number
   range: number
-  type:
-    | 'round'
-    | 'set'
-    | 'all'
-    | 'first'
-    | 'last'
-    | 'nearest'
-    | 'furthest'
-    | 'weakest'
-    | 'strongest'
-  setAngle?: number
   delay: number
   size: number
   projectiles: number
@@ -24,4 +15,21 @@ export interface AttackInterface {
     camouflage: boolean
     iron: boolean
   }
+}
+
+interface setAttackInterface extends basicAttackInterface {
+  type: 'set'
+  setAngles: number[]
+}
+
+interface trackingAttackInterface extends basicAttackInterface {
+  type:
+    | 'round'
+    | 'all'
+    | 'first'
+    | 'last'
+    | 'nearest'
+    | 'furthest'
+    | 'weakest'
+    | 'strongest'
 }
